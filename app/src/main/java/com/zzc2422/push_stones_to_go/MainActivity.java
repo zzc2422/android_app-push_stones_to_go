a6cc5aa898034734e5e1a367c974fc30859e518b
zzc2422 <liksasdilasely@163.com>
2018-02-08 12:16:01 +0800

    初始化

---
+++ app/src/main/java/com/zzc2422/push_stones_to_go/MainActivity.java
@@ -0,0 +1,12 @@
+package com.zzc2422.push_stones_to_go;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
